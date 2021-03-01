65ae7805165bfc05c1939223720dcbb009f394a8
nocey <naci.ayti@ogr.dpu.edu.tr>
2021-03-01 23:48:45 +0300

    Change the navigation
    navigation framework change by the react navigator becouse beter than react router

--- App.tsx
+++ App.tsx
@@ -1,42 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Nav from './components/Nav/Nav';
-import { NativeRouter, Route} from "react-router-native";
 import Home from './components/Home/Home';
 import Profile from './components/Profile/Profile';
 import Todo from './components/Todo/Todo';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 
+const Tab = createBottomTabNavigator();
 
-export default function App() {
+export default function MyTabs() {
   return (
-    <NativeRouter>
-    <View style={styles.container}>
-      <Route exact path='/' component={Home}></Route>
-      <Route path='/profile' component={Profile}></Route>
-      <Route path='/todo' component={Todo}></Route>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Nav/>
-      <StatusBar style="auto" />
-    </View>
-    </NativeRouter>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Todo" component={Todo} />
+        <Tab.Screen name="Profile" component={Profile} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding:0,
-    margin:0,
-
-  },
-  text:{
-    backgroundColor: 'black'
-  },
-
-});
 
 
--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -4,11 +4,25 @@ import { Text, StyleSheet, View } from 'react-native'
 export default class Home extends Component {
     render() {
         return (
-            <View>
+            <View style={styles.container}>
                 <Text> Home </Text>
             </View>
         )
     }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 0,
+      margin: 0,
+  
+    },
+    text: {
+      backgroundColor: 'black'
+    },
+  
+  });
